572e23c3a5c56e7a502f05dda108973385f74ae2
bd <bd@DESKTOP-C9M4VTO>
2022-12-26 05:53:47 +0900

    d

--- src/main/java/com/byung8/fitness/exercise/SwaggerConfig.java
+++ src/main/java/com/byung8/fitness/exercise/SwaggerConfig.java
@@ -16,8 +16,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any()) // ���� RequestMapping���� �Ҵ�� ��� API ����Ʈ
-                .paths(PathSelectors.any()) // ��� API ����
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 }
